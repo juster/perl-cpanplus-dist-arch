d16d1a45fa711698fd73c98801b476d7a570ca52
Justin Davis <jrcd83@gmail.com>
2010-05-10 16:31:28 -0700

    Revert "Change package naming rules.  Always prepend with perl-."
    This reverts commit 1bbca16c9823dddec1a5ef023a12f7cbf5640d74.

--- lib/CPANPLUS/Dist/Arch.pm
+++ lib/CPANPLUS/Dist/Arch.pm
@@ -480,8 +480,10 @@ sub dist_pkgname
     die qq{Dist name '$distname' completely violates packaging standards}
         if ( ! $distname );
 
-    # Don't create a redundant 'perl-' prefix in the package name...
-    $distname = "perl-$distname" unless $distname =~ /\Aperl-/;
+    if ( $distname !~ / (?: \A perl ) | (?: -perl \z ) /xms ) {
+        $distname = "perl-$distname";
+    }
+
     return $distname;
 }
 
