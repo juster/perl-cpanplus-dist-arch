06d2079bcf610fc4a6d15ae28b051ce43712a9b4
Justin Davis <jrcd83@gmail.com>
2010-06-02 20:58:17 -0700

    Checking in changes prior to tagging of version 1.04.
    Changelog diff is:

--- lib/CPANPLUS/Dist/Arch.pm
+++ lib/CPANPLUS/Dist/Arch.pm
@@ -21,7 +21,7 @@ use English                qw(-no_match_vars);
 use Carp                   qw(carp croak confess);
 use Cwd                    qw();
 
-our $VERSION     = '1.03';
+our $VERSION     = '1.04';
 our @EXPORT      = qw();
 our @EXPORT_OK   = qw(dist_pkgname dist_pkgver);
 our %EXPORT_TAGS = ( 'all' => [ @EXPORT_OK ] );
