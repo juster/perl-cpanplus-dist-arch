d7e9c1f150b53f24238d2651c6ebc8564c0ccb09
Justin Davis <jrcd83@gmail.com>
2011-08-03 20:25:32 -0400

    Checking in changes prior to tagging of version 1.16.
    Changelog diff is:

--- lib/CPANPLUS/Dist/Arch.pm
+++ lib/CPANPLUS/Dist/Arch.pm
@@ -6,7 +6,7 @@ use strict;
 use CPANPLUS::Dist::Base   qw();
 use Exporter               qw(import);
 
-our $VERSION     = '1.15';
+our $VERSION     = '1.16';
 our @EXPORT      = qw();
 our @EXPORT_OK   = qw(dist_pkgname dist_pkgver);
 our %EXPORT_TAGS = ( 'all' => [ @EXPORT_OK ] );
