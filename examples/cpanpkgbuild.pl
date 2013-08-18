71b73113d4e891289f86591615f3a95c13390570
Justin Davis <jrcd83@gmail.com>
2013-08-18 11:08:34 +0000

    Rewrote examples/cpanpkgbuild.pl.
    Instead of writing to a file we print the PKGBUILD to stdout.
    Removed strict and warnings because the script is so short.
    Rewrote the style to match my latest.

--- examples/cpanpkgbuild.pl
+++ examples/cpanpkgbuild.pl
@@ -1,81 +1,33 @@
 #!/usr/bin/perl
 
-use warnings;
-use strict;
-
 use CPANPLUS::Backend;
-use Getopt::Long;
-use Pod::Usage;
-use English '-no_match_vars';
 
-BEGIN {
-    eval { require CPANPLUS::Dist::Arch; }
-        or die 'CPANPLUS::Dist::Arch must be installed for this script to work.'
+if(@ARGV < 1){
+    print STDERR "usage: cpanpkgbuild.pl [module]\n";
+    exit 2;
 }
 
-my $force;
-GetOptions( force => \$force ); # must be before messing with @ARGV
+$m = shift;
+if(!eval { require CPANPLUS::Dist::Arch; }){
+    print STDERR "error: failed to load CPANPLUS::Dist::Arch\n";
+    exit 1;
+}
 
-my $modname = shift @ARGV or pod2usage;
+$cb = new CPANPLUS::Backend;
+$x  = $cb->module_tree($m);
+if(!$x){
+    print STDERR "error: module not found: $m\n";
+    exit 100;
+}
 
-# Check if a PKGBUILD already exists...
-die << 'END_ERROR' if ( -f 'PKGBUILD' && !$force );
-PKGBUILD already exists in current directory.  Use -f to force overwrite.
-END_ERROR
+$cb->configure_object->set_conf('prereqs' => 0);
+$x->fetch('verbose' => 0);
+$x->extract('verbose' => 0);
+$y = $x->dist('target' => 'prepare', 'format' => 'CPANPLUS::Dist::Arch');
+if(!$y){
+    print STDERR "error: failed to prepare distribution\n";
+    exit 1;
+}
 
-my $cb      = CPANPLUS::Backend->new;
-my $modobj  = $cb->module_tree($modname)
-    or die qq{module '$modname' not found in CPANPLUS};
-
-# Try to avoid interactive requests...
-$cb->configure_object->set_conf( 'prereqs', 0 );
-$ENV{PERL_AUTOINSTALL} = '--skipdeps';
-
-# Prepare the package, but don't install it...
-$modobj->fetch  ( verbose => 0 );
-$modobj->extract( verbose => 0 );
-my $distobj = $modobj->dist( target => 'prepare',
-                             format => 'CPANPLUS::Dist::Arch' )
-    or die q{failed to prepare distribution object};
-
-# Get the PKGBUILD from CPANPLUS::Dist::Arch and print to a file...
-my $pkgbuild_txt = $distobj->get_pkgbuild()
-    or die q{ERROR get_pkgbuild() returned an empty string};
-
-open my $pkgbuild_file, '>', 'PKGBUILD'
-    or die qq{ERROR creating PKGBUILD: $OS_ERROR};
-print $pkgbuild_file $pkgbuild_txt;
-close $pkgbuild_file
-    or die qq{ERROR closing new PKGBUILD: $OS_ERROR};
-
-print "Created PKGBUILD.\n";
-
+print $y->get_pkgbuild();
 exit 0;
-
-__END__
-
-=head1 NAME
-
-cpanpkgbuild.pl - Create a PKGBUILD file for a perl module
-
-=head1 SYNOPSIS
-
-cpanpkgbuild.pl [-f] DBD::SQLite
-
-Will create a PKGBUILD in the current directory for DBD::SQLite.  Then
-use your favorite editor to customize PKGBUILD, run makepkg on it,
-etc. etc.  The -f or -force flag forces overwriting any existing PKGBUILD.
-
-=head1 AUTHOR
-
-Justin Davis, C<< <jrcd83 at gmail.com> >>, juster on
-L<http://bbs.archlinux.org>
-
-=head1 COPYRIGHT & LICENSE
-
-Copyright 2009 Justin Davis, all rights reserved.
-
-This program is free software; you can redistribute it and/or modify it
-under the same terms as Perl itself.
-
-=cut
